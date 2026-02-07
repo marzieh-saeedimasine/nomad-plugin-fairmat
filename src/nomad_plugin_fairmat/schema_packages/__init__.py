489ce08a23775bc0e48b8d01f8a8e26922aff210
marzieh-saeedimasine <marzieh.saeedimasine@gmail.com>
2026-02-07 19:11:47 +0000

    Added sintering classes from yaml schema

--- src/nomad_plugin_fairmat/schema_packages/__init__.py
+++ src/nomad_plugin_fairmat/schema_packages/__init__.py
@@ -1,17 +1,15 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
-from pydantic import Field
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
+class SinteringEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
-        from nomad_plugin_fairmat.schema_packages.schema_package import m_package
+        from nomad_plugin_fairmat.schema_packages.sintering import m_package
 
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+sintering = SinteringEntryPoint(
+    name='Sintering',
+    description='Schema package for describing a sintering process.',
 )
